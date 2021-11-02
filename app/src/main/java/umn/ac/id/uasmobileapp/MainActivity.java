c327447ff913f89959bb06d3e091e1b96977670f
Kakaranara <wahyu.koco@student.umn.ac.id>
2021-11-02 19:51:52 +0700

    adding bg

--- app/src/main/java/umn/ac/id/uasmobileapp/MainActivity.java
+++ app/src/main/java/umn/ac/id/uasmobileapp/MainActivity.java
@@ -1,6 +1,7 @@
 package umn.ac.id.uasmobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO); //no dark mode
 
         //Find ID
         btnLoginPage = findViewById(R.id.btnLoginPage);
