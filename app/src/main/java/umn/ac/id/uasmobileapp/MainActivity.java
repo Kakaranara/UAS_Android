81d300747ee691974bfe82851879800673f301b8
Kakaranara <wahyu.koco@student.umn.ac.id>
2021-11-02 15:56:09 +0700

    error fixed + intent
    nb : drawable gak boleh pake huruf besar

--- app/src/main/java/umn/ac/id/uasmobileapp/MainActivity.java
+++ app/src/main/java/umn/ac/id/uasmobileapp/MainActivity.java
@@ -2,13 +2,33 @@ package umn.ac.id.uasmobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnLoginPage, btnRegisterPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find ID
+        btnLoginPage = findViewById(R.id.btnLoginPage);
+        btnRegisterPage = findViewById(R.id.btnRegisterPage);
+
+        //Handling OnClick
+        btnLoginPage.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this,Login.class);
+            startActivity(intent);
+        });
+
+        btnRegisterPage.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, Register.class);
+            startActivity(intent);
+        });
     }
 }
