499ac5d361fe6bae936ef6493501b5ca006cf965
Intrigon <73568756+Intrigon@users.noreply.github.com>
2021-11-28 16:52:56 +0700

    ui payment

---
+++ app/src/main/java/umn/ac/id/uasmobileapp/AdminPembayaranFragment.java
@@ -0,0 +1,14 @@
+package umn.ac.id.uasmobileapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AdminPembayaranFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_admin_pembayaran);
+    }
+}
