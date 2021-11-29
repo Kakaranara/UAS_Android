e7fce40a639311de1ae6881ca3cc6966611478c1
Kakaranara <wahyu.koco@student.umn.ac.id>
2021-11-29 15:37:54 +0700

    nama bisnis user from key session

--- app/src/main/java/umn/ac/id/uasmobileapp/UserActivity.java
+++ app/src/main/java/umn/ac/id/uasmobileapp/UserActivity.java
@@ -1,5 +1,6 @@
 package umn.ac.id.uasmobileapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
@@ -7,27 +8,58 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Debug;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.LinkedList;
 
 public class UserActivity extends AppCompatActivity {
     ImageButton btnHome;
-    TextView namaBisnis;
+    TextView tvNamaBisnis;
+    String namaBisnis;
+    Session session;
 //    Constraints navbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
-        namaBisnis = findViewById(R.id.nama_bisnis);
+        tvNamaBisnis = findViewById(R.id.nama_bisnis);
+        session = new Session(getApplicationContext());
+        String key = session.getKey();
 
-        Bundle extras = getIntent().getExtras();
-        String bName = extras.getString("businessName");
-        namaBisnis.setText(bName);
-        Toast.makeText(UserActivity.this, "Business name: " + bName, Toast.LENGTH_SHORT).show();
+        DatabaseReference reference = FirebaseDatabase.getInstance("https://final-project-mobile-app-98d46-default-rtdb.firebaseio.com/").getReference("business");
+        Query query = reference.orderByChild("Employee/" + key).equalTo(true);
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    for(DataSnapshot businessSnapshot : dataSnapshot.getChildren()){
+                        namaBisnis = businessSnapshot.child("business_name").getValue(String.class);
+                    }
+                    tvNamaBisnis.setText(namaBisnis);
+                }
+                else{
+                    Log.d("Datasnapshot", "onDataChange: NULL");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
         if(savedInstanceState == null){
             getSupportFragmentManager()
