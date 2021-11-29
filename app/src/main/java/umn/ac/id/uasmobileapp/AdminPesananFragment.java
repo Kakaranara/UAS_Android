dacdf654ec5593b7ac6cf1360d6b14dcc598d2bf
Anthony Rafael <anthony.rafael@student.umn.ac.id>
2021-11-29 18:45:19 +0700

    Admin Pesanan Temporary (Belum Muncul)

--- app/src/main/java/umn/ac/id/uasmobileapp/AdminPesananFragment.java
+++ app/src/main/java/umn/ac/id/uasmobileapp/AdminPesananFragment.java
@@ -2,18 +2,29 @@ package umn.ac.id.uasmobileapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdminPesananFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class AdminPesananFragment extends Fragment {
+    private View view;
+    private RecyclerView recyclerView;
+    DatabaseReference mbase, productRef;
+
+
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -28,37 +39,106 @@ public class AdminPesananFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminPesananFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AdminPesananFragment newInstance(String param1, String param2) {
-        AdminPesananFragment fragment = new AdminPesananFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+
+//    // TODO: Rename and change types and number of parameters
+//    public static AdminPesananFragment newInstance(String param1, String param2) {
+//        AdminPesananFragment fragment = new AdminPesananFragment();
+//        Bundle args = new Bundle();
+//        args.putString(ARG_PARAM1, param1);
+//        args.putString(ARG_PARAM2, param2);
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_admin_pesanan, container, false);
+        view = inflater.inflate(R.layout.fragment_admin_barang, container, false);
+
+        mbase = FirebaseDatabase.getInstance("https://final-project-mobile-app-98d46-default-rtdb.firebaseio.com/").getReference().child("products");
+
+        recyclerView = (RecyclerView) view.findViewById(R.id.rvOrder);
+
+        // To display the Recycler view linearly
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerOptions<Order> options
+                = new FirebaseRecyclerOptions.Builder<Order>()
+                .setQuery(mbase, Order.class)
+                .build();
+
+        FirebaseRecyclerAdapter<Order, AdminPesananFragment.AdminPesananViewholder> adapter
+                = new FirebaseRecyclerAdapter<Order, AdminPesananFragment.AdminPesananViewholder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull AdminPesananViewholder holder, int position, @NonNull Order model) {
+                    final String product_name = getRef(position).getKey();
+
+                    holder.product_name.setText(product_name);
+
+                    //DatabaseReference getTypeRef = getRef(position).;
+
+    //                        mbase.orderByChild("business_id").equalTo(currentUser).addValueEventListener(new ValueEventListener() {
+    //                            @Override
+    //                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+    //                                if(dataSnapshot.exists()){
+    //                                    String price = dataSnapshot.child("price").getValue().toString();
+    //                                    holder.product_price.setText(price);
+    //                                }
+    //                            }
+    //
+    //                            @Override
+    //                            public void onCancelled(@NonNull DatabaseError databaseError) {
+    //
+    //                            }
+    //                        });
+
+                    //holder.product_quantity.setText(model.getStock());
+                    holder.editBtn.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            //showEditDialog();
+                        }
+                    });
+                }
+
+                @NonNull
+                @Override
+                public AdminPesananFragment.AdminPesananViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                    View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.product_list_admin, parent, false);
+                    return new AdminPesananFragment.AdminPesananViewholder(view);
+                }
+            };
+
+        recyclerView.setAdapter(adapter);
+        adapter.startListening();
+    }
+
+    public static class AdminPesananViewholder extends RecyclerView.ViewHolder {
+        TextView product_name, product_price, product_quantity;
+        Button editBtn;
+        public AdminPesananViewholder(@NonNull View itemView)
+        {
+            super(itemView);
+            product_name = itemView.findViewById(R.id.product_name);
+            product_price = itemView.findViewById(R.id.product_price);
+            product_quantity = itemView.findViewById(R.id.prduct_quantity);
+            editBtn = itemView.findViewById(R.id.editBtn);
+        }
     }
 }
---
+++ app/src/main/java/umn/ac/id/uasmobileapp/Order.java
@@ -0,0 +1,31 @@
+package umn.ac.id.uasmobileapp;
+
+public class Order {
+    private String account_id, order_datetime, status;
+
+    public Order(){}
+
+    public String getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(String account_id) {
+        this.account_id = account_id;
+    }
+
+    public String getOrder_datetime() {
+        return order_datetime;
+    }
+
+    public void setOrder_datetime(String order_datetime) {
+        this.order_datetime = order_datetime;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
