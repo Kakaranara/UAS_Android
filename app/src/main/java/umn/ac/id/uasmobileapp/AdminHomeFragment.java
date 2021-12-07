117778a8f969bb5d71e4c501490214a37ed98a4d
Kakaranara <wahyu.koco@student.umn.ac.id>
2021-12-07 12:29:31 +0700

    home sementara

--- app/src/main/java/umn/ac/id/uasmobileapp/AdminHomeFragment.java
+++ app/src/main/java/umn/ac/id/uasmobileapp/AdminHomeFragment.java
@@ -2,63 +2,89 @@ package umn.ac.id.uasmobileapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AdminHomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
 public class AdminHomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TextView tvTotalRevenue, tvTotalDish, tvTotalCustomer;
+    Session session;
+    DatabaseReference orderRef, cartRef;
+    ArrayList<String> total_customer = new ArrayList<>();
 
     public AdminHomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AdminHomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AdminHomeFragment newInstance(String param1, String param2) {
-        AdminHomeFragment fragment = new AdminHomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        session = new Session(getContext());
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
+
+
         return inflater.inflate(R.layout.fragment_admin_home, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        tvTotalCustomer = getView().findViewById(R.id.AdminHomeTotalCustomer);
+        tvTotalRevenue = getView().findViewById(R.id.homeAdminTotalRevenue);
+        tvTotalDish = getView().findViewById(R.id.homeAdminTotalDish);
+
+        cartRef = FirebaseDatabase.getInstance("https://final-project-mobile-app-98d46-default-rtdb.firebaseio.com/").getReference().child("carts");
+        orderRef = FirebaseDatabase.getInstance("https://final-project-mobile-app-98d46-default-rtdb.firebaseio.com/").getReference().child("orders");
+
+        orderRef.orderByChild("business_id").equalTo(session.getBusinessKey()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot orderSnapshot : snapshot.getChildren()){
+                    total_customer.add(orderSnapshot.getKey());
+                }
+                tvTotalCustomer.setText("" + total_customer.toArray().length);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        //TODO : bikin total revenue + dish
+        cartRef.orderByChild("business_id").equalTo(session.getBusinessKey()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 }
